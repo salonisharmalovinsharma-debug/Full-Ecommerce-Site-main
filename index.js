c3eec703621e08885d34f2f30e3647d57a176c1e
salonisharmalovinsharma-debug <salonisharmalovinsharma@gmail.com>
2025-09-02 14:24:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
-const connectDatabase = require("./config/database");
-const cloudinary = require("cloudinary");
-const path = require("path");
-const errorMiddleware = require("./middleware/error");
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const app = express();
-dotenv.config();
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(fileUpload());
+const options = {
+    timeout: 5000,
+    position: positions.BOTTOM_CENTER,
+    transition: transitions.SCALE,
+};
 
-// import all routes
-const auth = require("./routes/auth");
-const products = require("./routes/product");
-const payment = require("./routes/payment");
-const order = require("./routes/order");
+ReactDOM.render(
+    <Provider store={store}>
+        <AlertProvider template={AlertTemplate} {...options}>
+            <App />
+        </AlertProvider>
+    </Provider>,
+    document.getElementById("root")
+);
 
-app.use("/api/v1", auth);
-app.use("/api/v1", products);
-app.use("/api/v1", payment);
-app.use("/api/v1", order);
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-});
-
-// connecting to database
-connectDatabase();
-
-// Setting up cloudinary configuration
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-app.use("/", (req, res) => {
-    res.send("App is running.");
-});
-
-// Middleware to handle error
-app.use(errorMiddleware);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-});
+reportWebVitals();
